6f9a3a5c3adc042950c184afff84219fc47c7a42
Eric Mizukami <eric.toshio@usp.br>
2022-12-04 16:03:43 -0300

    Ajustar tela de cadastro

--- user/forms.py
+++ user/forms.py
@@ -7,24 +7,24 @@ from django.contrib.auth import get_user_model
 class AlunoCadastroForm(UserCreationForm):
 
     # Padrão todos os usuários
-    nome = forms.CharField(label='Nome*', required = True)
+    nome = forms.CharField(label='Nome*', required = True, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     # Campos comuns com o professor
-    cpf = forms.CharField(label='CPF*',required = True)
-    email = forms.EmailField(label='Email*', required = True)
-    nascimento = forms.DateField(label='Data de nascimento (dd/mm/aaaa)*', widget = forms.DateInput(format='%d/%m/%Y', attrs={'type':'date'}), required = True)
-    telefone = forms.CharField(label='Número de telefone*',required = True)
-    endereco = forms.CharField(label='Endereço',required = False)
-    urlfoto = forms.CharField(label='Url da Foto*',required = True)
+    cpf = forms.CharField(label='CPF*',required = True, widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(label='Email*', required = True, widget=forms.EmailInput(attrs={'class':'form-control'}))
+    nascimento = forms.DateField(label='Data de nascimento*', widget = forms.DateInput(format='%d/%m/%Y', attrs={'type':'date', 'class':'form-control'}), required = True)
+    telefone = forms.CharField(label='Número de telefone*',required = True, widget=forms.TextInput(attrs={'class':'form-control'}))
+    endereco = forms.CharField(label='Endereço',required = False, widget=forms.TextInput(attrs={'class':'form-control'}))
+    urlfoto = forms.CharField(label='Url da Foto*',required = True, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     # Campos exclusivos/perfil
-    altura = forms.IntegerField(label='Altura (cm)',required = False)
-    peso = forms.IntegerField(label='Peso (kg)',required = False)
-    bracos = forms.IntegerField(label='Braços (cm)',required = False)
-    coxa = forms.IntegerField(label='Coxa (cm)',required = False)
-    peitoral = forms.IntegerField(label='Peitoral (cm)',required = False)
-    cinturaescapular = forms.IntegerField(label='Cintura escapular (cm)',required = False)
-    percentualgordura = forms.IntegerField(label='Percentual de gordura (%)',required = False)
+    altura = forms.IntegerField(label='Altura (cm)',required = False, widget=forms.NumberInput(attrs={'class':'form-control'}))
+    peso = forms.IntegerField(label='Peso (kg)',required = False, widget=forms.NumberInput(attrs={'class':'form-control'}))
+    bracos = forms.IntegerField(label='Braços (cm)',required = False, widget=forms.NumberInput(attrs={'class':'form-control'}))
+    coxa = forms.IntegerField(label='Coxa (cm)',required = False, widget=forms.NumberInput(attrs={'class':'form-control'}))
+    peitoral = forms.IntegerField(label='Peitoral (cm)',required = False, widget=forms.NumberInput(attrs={'class':'form-control'}))
+    cinturaescapular = forms.IntegerField(label='Cintura escapular (cm)',required = False, widget=forms.NumberInput(attrs={'class':'form-control'}))
+    percentualgordura = forms.IntegerField(label='Percentual de gordura (%)',required = False, widget=forms.NumberInput(attrs={'class':'form-control'}))
     secretario = forms.ModelChoiceField(label='Secretário responsável',queryset = Secretario.objects.all(), widget = forms.Select, required=True)
 
     class Meta(UserCreationForm.Meta):
@@ -65,18 +65,18 @@ class AlunoCadastroForm(UserCreationForm):
 class ProfessorCadastroForm(UserCreationForm):
 
     # Padrão todos os usuários
-    nome = forms.CharField(label='Nome*', required = True)
+    nome = forms.CharField(label='Nome*', required = True, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     # Campos comuns com o professor
-    cpf = forms.CharField(label='CPF*', required = True)
-    email = forms.EmailField(label='Email*', required = True)
+    cpf = forms.CharField(label='CPF*', required = True, widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(label='Email*', required = True, widget=forms.EmailInput(attrs={'class':'form-control'}))
     # idade = forms.IntegerField(required = True)
     # telefone = forms.CharField(required = True)
     # endereco = forms.CharField(required = True)
-    urlfoto = forms.CharField(label='URL da foto*',required = True)
+    urlfoto = forms.CharField(label='URL da foto*',required = True, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     # # Campos exclusivos
-    cref = forms.CharField(label='CREF*',required = True)
+    cref = forms.CharField(label='CREF*',required = True, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
